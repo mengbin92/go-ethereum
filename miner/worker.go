6da3ae8659b2c6bf296f756d54dec247f0f45493
mengbin92 <mengbin1992@outlook.com>
2024-10-17 16:15:24 +0800

    fix(使用env.header.Number.Uint64() > 1临时解决区块1没有出块奖励的问题):

--- miner/worker.go
+++ miner/worker.go
@@ -1184,7 +1184,7 @@ func (w *worker) commit(env *environment, interval func(), update bool, start ti
 		// Create a local environment copy, avoid the data race with snapshot state.
 		// https://github.com/ethereum/go-ethereum/issues/24299
 		env := env.copy()
-		if env.tcount < int(w.chainConfig.Clique.MinTxs) {
+		if env.header.Number.Uint64() > 1 && env.tcount < int(w.chainConfig.Clique.MinTxs) {
 			log.Info("Not enough transactions in pool to fill block", "MinTxCount", w.chainConfig.Clique.MinTxs)
 			return errors.New("not enough transactions in pool to fill block")
 		}
